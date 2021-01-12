dff05352383615dab4625afb58cc73888154817a
jDanielDetes <jdanieldetes@gmail.com>
2021-01-12 14:55:42 -0500

    Movie details page styled

--- app/src/main/java/com/onramp/android/takehome/movie_details.kt
+++ app/src/main/java/com/onramp/android/takehome/movie_details.kt
@@ -5,14 +5,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import com.bumptech.glide.Glide
 
 import com.onramp.android.takehome.model.MovieAdapter
+import org.w3c.dom.Text
 
 
 class movie_details() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
 
 
             //Recieve intents
@@ -27,7 +31,16 @@ class movie_details() : AppCompatActivity() {
         val movie_backdrop = intent.getStringExtra(MovieAdapter.MovieViewHolder.MOVIE_BACKDROP_KEY)
         val rating = intent.getStringExtra(MovieAdapter.MovieViewHolder.MOVIE_VOTE_KEY)
 
+        Glide.with(this)
+                .load("https://image.tmdb.org/t/p/w1280/" + movie_backdrop)
+                .into(findViewById(R.id.imageView2))
 
+
+        val overView:TextView =findViewById(R.id.overview)
+        val titleView:TextView =findViewById(R.id.Title)
+
+        overView.text = overview
+        titleView.text=navtitle
 
 
 
